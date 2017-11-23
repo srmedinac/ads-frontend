c3f8121abe947e82a23caa4b47e09a07f544b1a6
Degly Sebastián Pava Pava <dspavap@unal.edu.co>
2017-11-22 21:46:57 -0500

    token

--- src/app/services/authentication/authentication.service.ts
+++ src/app/services/authentication/authentication.service.ts
@@ -10,6 +10,26 @@ export class AuthenticationService {
    loginURL='https://192.168.99.101:3011/login'
   constructor(private http: Http) { }
 
+    login(username: string, password: string) {
+        return this.http.post('/api/authenticate', JSON.stringify({ username: username, password: password }))
+            .map((response: Response) => {
+                // login successful if there's a jwt token in the response
+                let user = response.json();
+                if (user && user.token) {
+                    // store user details and jwt token in local storage to keep user logged in between page refreshes
+                    localStorage.setItem('currentUser', JSON.stringify(user));
+                }
+ 
+                return user;
+            });
+    }
+ 
+    logout() {
+        // remove user from local storage to log user out
+        localStorage.removeItem('currentUser');
+    }
+
+
   submitDataSignUp(name,username,email,birth,password){
     let data={
 
